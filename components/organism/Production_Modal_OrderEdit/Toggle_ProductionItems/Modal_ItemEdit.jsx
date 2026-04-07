7be38893256958b9c6d9bf47d2e0b04469374cd2
zluoDiamond <zluo@centra.ca>
2026-04-07 11:23:59 -0700

    enable

--- components/organism/Production_Modal_OrderEdit/Toggle_ProductionItems/Modal_ItemEdit.jsx
+++ components/organism/Production_Modal_OrderEdit/Toggle_ProductionItems/Modal_ItemEdit.jsx
@@ -124,7 +124,7 @@ const WINDOW_FIELDS = applyField([
   {
     Component: Editable.EF_Checkbox_Yesno,
     id: "Shaped",
-    // disabled: true, // 20260406: not editable from OM
+    //  disabled: true, // 20260406: not editable from OM
   },
 ]);
 
